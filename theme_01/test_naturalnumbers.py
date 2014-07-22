39935242b2ef54748d22ca3d6741512a50264274
omeH <hanulllu@gmail.com>
2014-07-23 00:40:40 +0300

    Correcting test->main v2

--- theme_01/test_naturalnumbers.py
+++ theme_01/test_naturalnumbers.py
@@ -8,15 +8,36 @@ from naturalnumbers import multiples
 def main():
     """Function main
     """
-    values = [0, 1, 10, 100, -1, 'spam', []]
-    for i in values:
-        try:
-            sum_ = sum(multiples(i))
-        except:
-            print('Bad value: {}'.format(i))
-        else:
-            if sum_ != 0:
-                print('Expected value is {}, actual is {}'.format(i, sum_))
+    # Data 0, expected value 0
+    sum_1 = sum(multiples(0))
+    if sum_1 != 0:
+        print('Expected value is {}, actual is {}'.format(0, sum_1))
+    # Data 1, expected value 0
+    sum_2 = sum(multiples(1))
+    if sum_2 != 0:
+        print('Expected value is {}, actual is {}'.format(0, sum_2))
+    # Data 10, expected value 23
+    sum_3 = sum(multiples(10))
+    if sum_3 != 23:
+        print('Expected value is {}, actual is {}'.format(23, sum_3))
+    # Data -1, expected value 0
+    sum_4 = sum(multiples(-1))
+    if sum_4 != 0:
+        print('Expected value is {}, actual is {}'.format(0, sum_4))
+    # Data -10, expected value 23
+    sum_5 = sum(multiples(-10))
+    if sum_5 != -23:
+        print('Expected value is {}, actual is {}'.format(-23, sum_5))
+    # Data 'spam', expected value <exception>
+    try:
+        sum_spam = sum(multiples('spam'))
+    except TypeError:
+        print('ERROR: value is {}'.format('spam'))
+    # Data [], expected value <exception>
+    try:
+        sum_list = sum(multiples([]))
+    except TypeError:
+        print('ERROR: value is {}'.format([]))
 
 
 if __name__ == '__main__':
