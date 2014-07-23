f408dd13deff69660f12f62b7123b2b544e207bc
omeH <hanulllu@gmail.com>
2014-07-23 21:56:11 +0300

    Add to read files

--- theme_02/fileread.py
+++ theme_02/fileread.py
@@ -10,9 +10,17 @@ Display the contents of the files produced on standart output.
 import sys
 
 def main():
+    """Function to read files
+    """
     if len(sys.argv) == 1:
         sys.stdout.write(sys.stdin.read())
         return
+    files = sys.argv[1:]
+    for file_ in files:
+        try:
+            sys.stdout.write(open(file_).read())
+        except IOError:
+            sys.stderr.write('File \'{}\' not found\n'.format(file_))
 
 
 if __name__ == '__main__':
