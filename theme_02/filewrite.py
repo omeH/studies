ca728b683f0f48b160a5cb5de7040c6f03dc174f
omeH <hanulllu@gmail.com>
2014-07-24 21:35:08 +0300

    Add function transfer

---
+++ theme_02/filewrite.py
@@ -0,0 +1,60 @@
+"""The module writes the data in file.
+
+Usage:
+    filewrite.py filename
+
+Description:
+    The module receives data from the standard input and writes
+    them to the specified file.
+"""
+
+import signal
+
+import sys
+
+BUFFER_SIZE = 256
+
+
+def sigint_handler(error, stack):
+    """Handler keystrokes CTRL+C
+    """
+    sys.stdout.write('\n')
+    sys.exit(1)
+
+
+def transfer(file_):
+    """Thia function reads data from a standard input blocks and
+    writes to file.
+    """
+    while True:
+        block = sys.stdin.read(BUFFER_SIZE)
+        if not block:
+            break
+        file_.write(block)
+
+
+def main():
+    """The main function
+    """
+    signal.signal(signal.SIGINT, sigint_handler)
+    errorstr = 'Filewrite.py: missing operand specifiels the file.\n' + \
+        'Get more informaition on the command: "filewrite.py -h\n'
+    if len(sys.argv) == 1:
+        sys.stderr.write(errorstr)
+        sys.exit(1)
+    if sys.argv[1] == '-h':
+        print(__doc__)
+        sys.exit(0)
+    try:
+        file_ = open(sys.argv[1], 'w')
+        transfer(file_)
+        file_.close()
+    except IOError as error:
+        sys.stderr.write('filewrite.py: {}\n'.format(error))
+    else:
+        sys.exit(0)
+    sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
