ab880df0ba1ae057eb2c64ab1a050d680f72cff0
omeH <hanulllu@gmail.com>
2014-07-29 11:42:46 +0300

    Correction logic

--- theme_04/test_sumdigits.py
+++ theme_04/test_sumdigits.py
@@ -1,7 +1,8 @@
 """Testing the module sudigits
 """
 
-from sumdigits import factorial, sum_digits, FactorialException
+from sumdigits import factorial, sum_digits
+from sumdigits import FactorialException, SumDigitsException
 
 
 def test_positive(data, value):
@@ -13,7 +14,16 @@ def test_positive(data, value):
               'actual is {}'.format(sum_))
 
 
-def test_exception(data, msg):
+def test_negative(data, value):
+    """Testing the sum_digits() with value negative
+    """
+    sum_ = sum_digits(data)
+    if sum_ != value:
+        print('Test_negative: Expected value is {}, '.format(value) +
+              'actual is {}'.format(sum_))
+
+
+def test_exception_factorial(data, msg):
     """Testing the factorial() to exception
     """
     try:
@@ -21,13 +31,26 @@ def test_exception(data, msg):
     except FactorialException:
         pass
     else:
-        print('Test exception: excepted \'{}\', '.format(msg) +
+        print('Test exception factorial: excepted \'{}\', '.format(msg) +
+              'but got no exception\n')
+
+
+def test_exception_sum_digits(data, msg):
+    """Testing the sum_digits() to exception
+    """
+    try:
+        sum_digits(data)
+    except SumDigitsException:
+        pass
+    else:
+        print('Test exception sum_digits: excepted \'{}\', '.format(msg) +
               'but got no exception\n')
 
 
 def main():
     """The main function
     """
+    # Test factorial()
     # Data 0, expected value 1
     test_positive(0, 1)
     # Data 5, expected value 3
@@ -35,11 +58,19 @@ def main():
     # Data 10, expected value 27
     test_positive(10, 27)
     # Data -10, expected value <exception>
-    test_exception(-10, 'negative int')
+    test_exception_factorial(-10, 'negative int')
     # Data 'str', expected value <exception>
-    test_exception('str', 'not int')
+    test_exception_factorial('str', 'not int')
     # Data [], expected value <exception>
-    test_exception([], 'not int')
+    test_exception_factorial([], 'not int')
+
+    # Test sum_digits()
+    # Data -10, excepted value 1
+    test_negative(-10, 1)
+    # Data 'str', expected value <exception>
+    test_exception_sum_digits('str', 'not int')
+    # Data [], expected value <exception>
+    test_exception_sum_digits([], 'not int')
 
 
 if __name__ == '__main__':
