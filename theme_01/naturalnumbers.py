bfea4fa569e15efdce9e70fe47443c681f483b14
omeH <hanulllu@gmail.com>
2014-08-06 11:44:52 +0300

    Fix logic progrm

--- theme_01/naturalnumbers.py
+++ theme_01/naturalnumbers.py
@@ -2,6 +2,14 @@
 """
 
 
+def check_natural_divisible(number, divisors):
+    """This function checks if number multiples of the divisors.
+    """
+    for divired in divisors:
+        if number % divired == 0:
+            return True
+
+
 def generate_natural_divisibles(limit, *divisors):
     """Generation a natural number multiples of the divisors.
     Numbers are generated up to the limit.
@@ -9,5 +17,5 @@ def generate_natural_divisibles(limit, *divisors):
     if limit < 0:
         return
     for number in range(1, limit):
-        if filter((lambda divider: number % divider == 0), divisors):
+        if check_natural_divisible(number, divisors):
             yield number
