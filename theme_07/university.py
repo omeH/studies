e46e0162b21779c443a54aef9d8995c16d9ac4c6
omeH <hanulllu@gmail.com>
2014-08-29 21:46:07 +0300

    Fix class Employee

--- theme_07/university.py
+++ theme_07/university.py
@@ -12,6 +12,14 @@ VIOLATIONS = {
 TAB = '    '
 
 
+def today():
+    return datetime.date.today()
+
+
+def timedelta(term):
+    return datetime.timedelta(days=term)
+
+
 class Unit(object):
     """
     >>> unit = Unit('rectorate', {})
@@ -51,11 +59,10 @@ class Unit(object):
     # Message format
     # --------------
     UNIT = 'Unit: {0}'
-    Unit_CELEBRATE = 'From the department {0} to celebrate the {1} present:'
+    UNIT_CELEBRATE = 'From the department {0} to celebrate the {1} present:'
     # --------------
 
-    name = ''
-    consist = {}
+    name = None
 
     def __init__(self, name, consist):
         self.name = name
@@ -78,7 +85,7 @@ class Unit(object):
         person.leave_job()
 
     def celebrate(self, what):
-        print(self.Unit_CELEBRATE.format(self.name, what))
+        print(self.UNIT_CELEBRATE.format(self.name, what))
         for position, persons in self.consist.iteritems():
             if not persons:
                 continue
@@ -126,10 +133,10 @@ class Person(object):
     name_key = ['Surname', 'Name', 'Patronymic']
     name = 'Incognito'
     _name = {key: 'Incognito' for key in name_key}
-    violations = []
 
     def __init__(self, name=None, **info):
         self.info = {}
+        self.violations = []
         if name:
             self.name = name
             self._name = dict(zip(self.name_key, name.split()))
@@ -152,7 +159,7 @@ class Person(object):
             print('{0}{1}'.format(TAB, person))
 
     def commit_violation(self, view):
-        self.violations.append((view, str(datetime.date.today())))
+        self.violations.append((view, str(today())))
 
     def talk(self, person, about):
         print(self.PERSON_TALK.format(self.name, person.name, about))
@@ -202,21 +209,32 @@ class Employee(Person):
         self.unit = unit
         self.unit.recruit(self.position, self)
 
-    def sign_contract(self, year=False, days=False):
+    def sign_contract(self, years=False, days=False):
         """
-        >>> unit = Unit('rectorate', {})
-        >>> emp = Employee('Ivanov Ivan', age=20, email='mail@mail.com')
+        >>> emp = Employee('Ivanov Ivan')
+        >>> emp.sign_contract() == None
+        True
+        >>> emp.sign_contract(years=3)
+        >>> emp.start_work == today()
+        True
+        >>> emp.end_work == emp.start_work + timedelta(3*YEAR)
+        True
+        >>> emp.sign_contract(days=30)
+        >>> emp.start_work == today()
+        True
+        >>> emp.end_work == emp.start_work + timedelta(30)
+        True
         """
-        if year is False and days is False:
-            return
-        term = year.real * YEAR + days.real
-        self.start_work = datetime.date.today()
-        self.end_work = self.start_work + datetime.timedelta(days=term)
+        if years is False and days is False:
+            return None
+        term = years.real * YEAR + days.real
+        self.start_work = today()
+        self.end_work = self.start_work + timedelta(term)
 
     def leave_job(self):
         """
         >>> unit = Unit('rectorate', {})
-        >>> emp = Employee('Ivanov Ivan', age=20, email='mail@mail.com')
+        >>> emp = Employee('Ivanov Ivan')
         >>> emp.get_job('secretary', 1000, unit)
         >>> emp.sign_contract(1)
         >>> emp.unit == unit, unit.consist['secretary'][0] == emp
@@ -228,7 +246,7 @@ class Employee(Person):
         """
         if self.unit is None:
             return
-        days_left = self.end_work - datetime.date.today()
+        days_left = self.end_work - today()
         print(self.EMPLOYEE_LEAVE.format(self.name, days_left.days))
         self.unit.exclude(self)
         self.unit = None
@@ -255,11 +273,23 @@ class Employee(Person):
     def go_vacation(self, term):
         """
         >>> emp = Employee('Ivanov Ivan')
+        >>> data = today() + timedelta(30)
+        >>> msg = emp.EMPLOYEE_VACATION.format(emp.name, data)
+        >>> emp.go_vacation(30) == msg
+        True
         """
         return self.EMPLOYEE_VACATION.format(
-            self.name,
-            datetime.date.today() + datetime.timedelta(days=term)
+            self.name, today() + timedelta(term)
         )
+
+    def absent_work(self):
+        """
+        >>> emp = Employee('Ivanov Ivan')
+        >>> emp.absent_work()
+        >>> emp.violations[0][0]
+        'absent from work'
+        """
+        self.commit_violation(VIOLATIONS['003'])
 
 
 if __name__ == '__main__':
