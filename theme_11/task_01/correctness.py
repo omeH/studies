8d0c36aff012926813e41100c9c3b3f2fa12ccde
omeH <hanulllu@gmail.com>
2014-10-11 11:07:32 +0300

    is_date_time: compilation re factored out of the function

--- theme_11/task_01/correctness.py
+++ theme_11/task_01/correctness.py
@@ -6,15 +6,16 @@ PATTERN = ''.join([
     r'(0[1-9]|1[012])-',        # Format month MM
     r'([0-2][0-9]|3[01]) ',     # Format day DD
     r'([01][0-9]|2[0-3])',      # Format hour HH
-    r'(:[0-5][0-9]){2,2}$'      # Format minute MM
+    r'(:[0-5][0-9]){2,2}$'      # Format minute MM and second SS
 ])
+
+RE_DATE_TIME = re.compile(PATTERN)
 
 
 def is_date_time(str_date_time):
     """Function check for the string str_date_time to the
     correct template 'YYYY-MM-DD HH:MM:SS'.
     """
-    regex = re.compile(PATTERN)
-    if regex.match(str_date_time):
+    if RE_DATE_TIME.match(str_date_time):
         return True
     return False
