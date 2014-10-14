da353ee77a56d9f532cf4cad4c0cedd05ee9e851
omeH <hanulllu@gmail.com>
2014-10-14 10:24:54 +0300

    Fix find_attrs, add help

--- theme_11/task_02/getattrsxml.py
+++ theme_11/task_02/getattrsxml.py
@@ -1,18 +1,18 @@
 import re
 import argparse
+import sys
 
 
 RE = r'<({0})?>(.*)</\1>'
-
-ATTR = 1
-VALUE = 2
 
 
 def parse_arg():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-f', '---file', type=str)
-    parser.add_argument('-l', '--list-attrs', type=str)
+    parser.add_argument('-f', '--file', type=str,
+                        help='Get the name of the file being processed')
+    parser.add_argument('-l', '--list-attrs', type=str,
+                        help='Get a list of attributes, separated by commas')
 
     return parser.parse_args()
 
@@ -24,16 +24,24 @@ def format_regex(list_attrs):
 
 def find_attrs(regex, xml_file):
     result = {}
+    group_attr = 1
+    group_value = 2
     for line in xml_file:
         obj = regex.search(line)
         if obj:
-            result[obj.group(ATTR)] = obj.group(VALUE)
+            result[obj.group(group_attr)] = obj.group(group_value)
     return result
 
 
 def main():
     options = parse_arg()
-    list_attrs = options.list_attrs.split(',')
+    if not options.file:
+        sys.stderr.write('--> No file name specified <--\n')
+        sys.exit(1)
+    if options.list_attrs:
+        list_attrs = options.list_attrs.split(',')
+    else:
+        list_attrs = []
     regex = re.compile(format_regex(list_attrs))
 
     with open(options.file) as xml_file:
