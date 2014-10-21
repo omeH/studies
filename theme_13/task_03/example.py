e6af858ca9c1110415f4ebf07363ce26aecbace6
omeH <hanulllu@gmail.com>
2014-10-21 11:14:26 +0300

    Add method: connect, delete, close

--- theme_13/task_03/example.py
+++ theme_13/task_03/example.py
@@ -5,7 +5,6 @@ import re
 import psycopg2
 
 
-DSN = 'dbname={0} user={1} password={2} host={3}'
 TABLE = 'example'
 COLUMNS = ['id', 'data', 'date', 'name']
 
@@ -16,16 +15,49 @@ class SQLInjection(Exception):
 
 class WorkWithDB(object):
 
-    sql_injection = re.compile(r'^[a-zA-Z\d]*$')
+    sql_injection = re.compile(r'^[a-zA-Z\d]*|(\d{4}(-\d{2}){2})$')
+    dsn_format = 'dbname={0} user={1} password={2} host={3}'
 
     SELECT_QUERY = 'SELECT {0} FROM {1};'
     INSERT_QUERY = 'INSERT INTO {0} ({1}) VALUES {2};'
     UPDATE_QUERY = 'UPDATE {0} SET {1} WHERE {2}'
     DELETE_QUERY = 'DELETE FROM {0} WHERE {1}'
 
-    def __init__(self, connection):
-        self.connection = connection
+    DBNAME_KEY = 'dbname'
+    USER_KEY = 'user'
+    PASSWORD_KEY = 'password'
+    HOST_KEY = 'host'
+
+    ###################
+    # Default options #
+    ###################
+    dbname = 'postgres'
+    user = 'postgres'
+    pasword = 'secret'
+    host = 'localhost'
+
+    connection = 0
+
+    def __init__(self, **options):
+        if self.DBNAME_KEY in options:
+            self.dbname = options[self.DBNAME_KEY]
+        if self.USER_KEY in options:
+            self.user = options[self.USER_KEY]
+        if self.PASSWORD_KEY in options:
+            self.pasword = options[self.PASSWORD_KEY]
+        if self.HOST_KEY in options:
+            self.host = options[self.HOST_KEY]
+
+    def connect(self):
+        dsn = self.dsn_format.format(
+            self.dbname, self.user, self.pasword, self.host
+        )
+        self.connection = psycopg2.connect(dsn)
         self.cursor = self.connection.cursor()
+
+    def close(self):
+        if self.connection:
+            self.connection.close()
 
     def _is_execute(self, query, **params):
         try:
@@ -64,9 +96,13 @@ class WorkWithDB(object):
         for item in data:
             self._correct_data(item)
 
-        values_list = [','.join(item) for item in data]
-        values = '({0})'.format(
-            '),('.join(values_list)
+        for i in xrange(len(data)):
+            for j in xrange(len(data[i])):
+                data[i][j] = "'{0}'".format(data[i][j])
+
+        values_list = [",".join(item) for item in data]
+        values = "({0})".format(
+            "),(".join(values_list)
         )
 
         query = self.INSERT_QUERY.format(
@@ -87,7 +123,7 @@ class WorkWithDB(object):
             data[index] = "{0} = '{1}'".format(*data[index])
 
         update_set = ','.join(data)
-        update_where = ' = '.join(condition)
+        update_where = "{0} = '{1}'".format(*condition)
 
         query = self.UPDATE_QUERY.format(table, update_set, update_where)
 
@@ -96,15 +132,24 @@ class WorkWithDB(object):
         else:
             self.connection.rollback()
 
-    def delete(self):
-        pass
+    def delete(self, table, condition):
+        self._correct_data([table] + condition)
+
+        delete_where = "{0} = '{1}'".format(*condition)
+
+        query = self.DELETE_QUERY.format(table, delete_where)
+
+        if self._is_execute(query):
+            self.connection.commit()
+        else:
+            self.connection.rollback()
 
 
 def parser_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        '-d', '--database', type=str, default='exampledb',
+        '-d', '--dbname', type=str, default='exampledb',
         help='Set a name database'
     )
     parser.add_argument(
@@ -126,24 +171,25 @@ def parser_args():
 def main():
     options = parser_args()
 
-    dsn = DSN.format(
-        options.database, options.user, options.password, options.host
-    )
-
     try:
-        con = psycopg2.connect(dsn)
-        w = WorkWithDB(con)
+        w = WorkWithDB(**options.__dict__)
+        w.connect()
         w.select(TABLE, COLUMNS)
-        w.insert(TABLE, ['test', 'value'], [['s', 't', 'd'], ['1', '2', '3']])
+        print
+        w.insert(TABLE, ['id', 'data', 'date', 'name'],
+                 [['8', 'test python', '21-10-2014', 'python']])
         w.select(TABLE, COLUMNS)
+        print
         w.update(TABLE, [['name', 'python']], ['id', '7'])
+        w.select(TABLE, COLUMNS)
+        print
+        w.delete(TABLE, ['id', '8'])
         w.select(TABLE, COLUMNS)
     except psycopg2.DatabaseError as error:
         print(error)
         sys.exit(1)
     finally:
-        if 'con' in globals():
-            con.close()
+        w.close()
 
 
 if __name__ == '__main__':
